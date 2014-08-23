72894f35c5d69fa22c025e16e540b5ffbf938d14
Edwin Steele <edwin-lists@wordspeak.org>
2014-08-23 17:29:38 +1000

    Cleanup output of loader

--- data/load_data_bundle.sh
+++ data/load_data_bundle.sh
@@ -7,16 +7,18 @@ psql -c "drop database jpharvest"
 echo -n "Dropping sil_rcem database... "
 psql -c "drop database sil_rcem"
 
-echo "Loading WALS"
+echo -n "Loading WALS... "
 psql -c "create database wals2013"
 psql wals2013 < wals.sql |
 	awk '/^(NOTICE|ERROR|WARN)/'
 
-echo "Loading JPHarvest schema"
+echo -n "Loading JPHarvest schema... "
 psql -c "create database jpharvest"
 psql jpharvest < JPHarvest.schema 2>&1 |
 	awk '/^(ERROR|WARN)/' |
-       	grep -v 'MSysNavPaneGroup'
+       	grep -v 'MSysNavPaneGroup' |
+	grep -v 'there is no unique constraint matching given keys for referenced table' |
+	grep -v 'constraint.*for relation.*already exists"
 
 echo "Loading JPHarvest data"
 psql jpharvest < JPHarvest.data 2>&1 |
@@ -24,10 +26,14 @@ psql jpharvest < JPHarvest.data 2>&1 |
   /INSERT 0 1/ {c++;};
   /^[^ISF]/ {print $0;};
   /^$/ {};
-  /^Finished/ {print $3, c, " inserts"; c=0;};'
+  /^Finished/ {print $3, c, "inserts"; c=0;};'
 
-echo "Loading SIL retired code element mappings (sil_rcem) schema"
+echo -n "Loading SIL retired code element mappings (sil_rcem) schema"
 psql -c "create database sil_rcem"
 psql sil_rcem < iso-639-3_Retirements.schema
 echo "Loading SIL retired code element mappings (sil_rcem) data"
-psql sil_rcem < iso-639-3_Retirements.tab
+# Exclude the header row (WITH HEADER not available in tab delim mode)
+grep -v 'Ret_Reason.*Ret_Remedy' iso-639-3_Retirements.tab | 
+	psql sil_rcem -c "copy Retirements from STDIN"
+
+echo "All done."
