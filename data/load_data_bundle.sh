8980b857ccf6547444d4914e4c2e33ebd6fd6709
Edwin Steele <edwin-lists@wordspeak.org>
2014-08-23 17:42:29 +1000

    Cleanup output, and don't raise errors if dbs don't exist

--- data/load_data_bundle.sh
+++ data/load_data_bundle.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 echo -n "Dropping wals2013 database... "
-psql -c "drop database wals2013"
+psql -c "DROP DATABASE IF EXISTS wals2013"
 echo -n "Dropping jpharvest database... "
-psql -c "drop database jpharvest"
+psql -c "DROP DATABASE IF EXISTS jpharvest"
 echo -n "Dropping sil_rcem database... "
-psql -c "drop database sil_rcem"
+psql -c "DROP DATABASE IF EXISTS sil_rcem"
 
 echo -n "Loading WALS... "
-psql -c "create database wals2013"
+psql -c "CREATE DATABASE wals2013"
 psql wals2013 < wals.sql |
 	awk '/^(NOTICE|ERROR|WARN)/'
 
 echo -n "Loading JPHarvest schema... "
-psql -c "create database jpharvest"
+psql -c "CREATE DATABASE jpharvest"
 psql jpharvest < JPHarvest.schema 2>&1 |
 	awk '/^(ERROR|WARN)/' |
        	grep -v 'MSysNavPaneGroup' |
@@ -28,8 +28,8 @@ psql jpharvest < JPHarvest.data 2>&1 |
   /^$/ {};
   /^Finished/ {print $3, c, "inserts"; c=0;};'
 
-echo -n "Loading SIL retired code element mappings (sil_rcem) schema"
-psql -c "create database sil_rcem"
+echo -n "Loading SIL retired code element mappings (sil_rcem) schema... "
+psql -c "CREATE DATABASE sil_rcem"
 psql sil_rcem < iso-639-3_Retirements.schema
 echo "Loading SIL retired code element mappings (sil_rcem) data"
 # Exclude the header row (WITH HEADER not available in tab delim mode)
