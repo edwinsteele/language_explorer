cea07c98815f9ef7e66afc5867c473f9f00bed7a
Edwin Steele <edwin-lists@wordspeak.org>
2014-09-12 15:38:59 +1000

    fix staging settings to reflect test vm

--- language_explorer/staging_settings.py
+++ language_explorer/staging_settings.py
@@ -4,8 +4,8 @@
 LANGUAGE_EXPLORER_DB_URL = 'postgresql://esteele@/language_explorer'
 JPHARVEST_DB_URL = 'postgresql://esteele@/jpharvest'
 WALS_DB_URL = 'postgresql://esteele@/wals2013'
-SIL_RCEM_TSV_SOURCE = '/Users/esteele/Code/language_explorer/data/iso-639-3_Retirements.tab'
-CENSUS_CSV_SOURCE  = '/Users/esteele/Code/language_explorer/data/census_2011_LANP_ENGLP.csv'
+SIL_RCEM_TSV_SOURCE = '/home/esteele/lex_data_bundle/iso-639-3_Retirements.tab'
+CENSUS_CSV_SOURCE  = '/home/esteele/lex_data_bundle/census_2011_LANP_ENGLP.csv'
 
-CACHE_ROOT = "/Users/esteele/lex_data_bundles/cache"
+CACHE_ROOT = "/home/esteele/lex_data_bundle/cache"
 TEST_CACHE_ROOT = CACHE_ROOT  # For the moment
