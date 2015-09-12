a0dfc730fa7cb45dca47dba9deaca4ac5540a5be
Edwin Steele <edwin-lists@wordspeak.org>
2015-09-12 17:48:55 +1000

    start own static server. handle static path/

--- make_static.sh
+++ make_static.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
 # Makes a static instance of the language explorer site.
+# Assumes it is not running.
 
+VENV_BASE="/Users/esteele/.virtualenvs/language_explorer"
+BASE_DIR="/Users/esteele/Code/language_explorer"
+STATIC_ASSET_BASE="$BASE_DIR/language_explorer"
+STATIC_ASSET_DIRNAME="static"
 MIRROR_OUTPUT_DIR="/Users/esteele/Sites/lex-mirror"
+LIBRARY_OUTPUT_DIR="$MIRROR_OUTPUT_DIR/lib"
 LEX_PROTOCOL="http://"
-LEX_INSTANCE="localhost:8000"
+LEX_INSTANCE="127.0.0.1:5000"
 
 rm -rf $MIRROR_OUTPUT_DIR
 mkdir -p $MIRROR_OUTPUT_DIR
+
+LANGUAGE_EXPLORER_DEPLOYMENT=staging $VENV_BASE/bin/gunicorn -b $LEX_INSTANCE language_explorer:app &
 
 pushd $MIRROR_OUTPUT_DIR
 wget --no-verbose --inet4-only --recursive --level=inf \
@@ -25,3 +33,11 @@ popd
 # We use gnu sed here so we have access to the -i argument
 find $MIRROR_OUTPUT_DIR -type f | \
 	xargs gsed -i 's/href="\(\/[a-z][a-z/]*\)">/href="\1.html">/'
+
+# Copy libraries
+mkdir $LIBRARY_OUTPUT_DIR
+pushd $STATIC_ASSET_BASE
+cp -R $STATIC_ASSET_DIRNAME $MIRROR_OUTPUT_DIR
+popd
+
+pkill -f gunicorn
