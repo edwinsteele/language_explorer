dfd02ec10a904c5ce52162fa512e0850c789ce32
Edwin Steele <edwin-lists@wordspeak.org>
2015-11-21 06:17:10 +1100

    Use staging for creating a static copy, so refer to in-place assets, not deployed assets

--- language_explorer/staging_settings.py
+++ language_explorer/staging_settings.py
@@ -5,7 +5,7 @@ LANGUAGE_EXPLORER_DB_URL = 'postgresql://esteele@/language_explorer'
 JPHARVEST_DB_URL = 'postgresql://esteele@/jpharvest'
 WALS_DB_URL = 'postgresql://esteele@/wals2013'
 SIL_RCEM_TSV_SOURCE = '/home/esteele/lex_data_bundle/iso-639-3_Retirements.tab'
-CENSUS_CSV_SOURCE  = '/home/esteele/lex_data_bundle/census_2011_LANP_ENGLP.csv'
+CENSUS_CSV_SOURCE = '/Users/esteele/Code/language_explorer/data/census_2011_LANP_ENGLP.csv'
 
 CACHE_ROOT = "/home/esteele/lex_data_bundle/cache"
 TEST_CACHE_ROOT = CACHE_ROOT  # For the moment
