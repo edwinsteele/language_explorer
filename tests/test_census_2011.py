3c77ffbfcd7c65350cfeba843daaaf6f112227ec
Edwin Steele <edwin-lists@wordspeak.org>
2015-12-11 12:27:50 +1100

    Fix census unit tests - gbc has been retired

--- tests/test_census_2011.py
+++ tests/test_census_2011.py
@@ -17,7 +17,6 @@ class TestCensus2011Adapter(BaseAdapterTestCase):
         p = LanguagePersistence(settings.LANGUAGE_EXPLORER_DB_URL)
         self.source = Census2011Adapter(settings.CENSUS_CSV_SOURCE, p)
 
-    @unittest.skip("Review - no initial thoughts about why this fails")
     def test_population_for_iso(self):
         iso_count_pairs = [
             ("gbb", 172),  # iso to single lang to single iso
@@ -26,16 +25,16 @@ class TestCensus2011Adapter(BaseAdapterTestCase):
             ("yii", constants.SPEAKER_COUNT_UNKNOWN),  # no speakers, but we
             # don't store data with less than SMALL_CELL_COUNT_THRESHOLD
             # speakers
-            ("gbc", constants.SPEAKER_COUNT_AMBIGUOUS),  # iso to single lang
-            # to 2 isos
             ("aer", constants.SPEAKER_COUNT_AMBIGUOUS),  # iso to 3 langs that
             # map to 2, 2 and 1 iso
         ]
         for iso, count in iso_count_pairs:
-            self.assertEqual(count,
-                             self.source.get_L1_speaker_count_for_iso(iso))
+            speaker_count = self.source.get_L1_speaker_count_for_iso(iso)
+            self.assertEqual(count, speaker_count,
+                             "Incorrect speaker count: %s. "
+                             "Got %s, expected %s" %
+                             (iso, speaker_count, count))
 
-    @unittest.skip("Review - no initial thoughts about why this fails")
     def test_english_competency(self):
         iso_competency_pairs = [
             ("gbb", (33, 83)),
@@ -44,11 +43,11 @@ class TestCensus2011Adapter(BaseAdapterTestCase):
                            constants.ENGLISH_COMPETENCY_UNKNOWN_OPTIMISTIC)),
             ("yiu", (constants.ENGLISH_COMPETENCY_UNKNOWN_PESSIMISTIC,
                      constants.ENGLISH_COMPETENCY_UNKNOWN_OPTIMISTIC)),
-            ("gbc", (72, 93)),
             ("aer", (45, 84)),
         ]
         for iso, competencies in iso_competency_pairs:
-            self.assertEqual(
-                competencies,
-                self.source.get_english_competency_percentages(iso)
-            )
+            ecp = self.source.get_english_competency_percentages(iso)
+            self.assertEqual(competencies, ecp,
+                             "Incorrect english competencies: %s. "
+                             "Got %s, expected %s" %
+                             (iso, ecp, competencies))
