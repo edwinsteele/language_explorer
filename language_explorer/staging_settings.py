a68be0dde70f5aa40fe1c2eb3f7a28c01710c1c6
Edwin Steele <edwin@wordspeak.org>
2019-07-20 17:56:04 +1000

    Update staging settings

--- language_explorer/staging_settings.py
+++ language_explorer/staging_settings.py
@@ -4,9 +4,8 @@
 LANGUAGE_EXPLORER_DB_URL = 'postgresql://esteele@/language_explorer'
 JPHARVEST_DB_URL = 'postgresql://esteele@/jpharvest'
 WALS_DB_URL = 'postgresql://esteele@/wals2013'
-SIL_RCEM_TSV_SOURCE = '/home/esteele/lex_data_bundle/iso-639-3_Retirements.tab'
-CENSUS_CSV_SOURCE = '/Users/esteele/Code/language_explorer/data/' \
-        'census_2011_LANP_ENGLP.csv'
+SIL_RCEM_TSV_SOURCE = '/Users/esteele/Code/lex_data/iso-639-3_Retirements.tab'
+CENSUS_CSV_SOURCE = '/Users/esteele/Code/lex_data/census_2011_LANP_ENGLP.csv'
 
-CACHE_ROOT = "/home/esteele/lex_data_bundle/cache"
+CACHE_ROOT = "/Users/esteele/Code/lex_data/cache"
 TEST_CACHE_ROOT = CACHE_ROOT  # For the moment
